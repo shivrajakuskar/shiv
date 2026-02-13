9e28eac5b4c0dc306e5eb2b99dc5c5233a9e229d
Shivraj Akuskar <shivrajakuskar7@gmail.com>
2026-02-14 04:10:42 +0530

    hello

---
+++ src/main/java/com/example/appa/AppaApplication.java
@@ -0,0 +1,13 @@
+package com.example.appa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppaApplication.class, args);
+	}
+
+}
